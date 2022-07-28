1a878d23d9d478a193ae4ae4b19a70087e743fed
Abere Allan <aabere70@gmail.com>
2022-07-28 17:36:55 +0300

    Updated the API code
    Still an issue with the response

--- package/index.js
+++ package/index.js
@@ -44,7 +44,7 @@ const generateToken = async (req, res, next) => {
         },
     }).then((response) => {
         // console.log(response.data.access_token);
-        const token = response.data.access_token;
+        token = response.data.access_token;
         next();
     }).catch((err) => {
         console.log(err);
@@ -54,37 +54,40 @@ const generateToken = async (req, res, next) => {
 
 
 app.post("/stk", generateToken, async (req, res) => {
-    const phoneNumber = req.body.phone.sibstring(-1); //ensure it starts with 254 eg. 254708374149
+    const date = new Date();
+
+    const phoneNumber = req.body.phone.substring(-1); //ensure it starts with 254 eg. 254708374149
     const amount = req.body.amount;
 
-    const businessNumber = process.env.MPESA_PAYBILL; //your paybill/tillnumber
-    const tillNumber = process.env.MPESA_TILL; //your paybill/tillnumber
-    const mpesaPassword = process.env.MPESA_PASSWORD; //your paybill/tillnumber
+    const businessNumber = process.env.MPESA_PAYBILL; //your paybill
+    const tillNumber = process.env.MPESA_TILL; //your tillnumber
+    const mpesaPassword = process.env.MPESA_PASSWORD; 
 
     const merchantEndPoint = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'; // change the sandbox to api during production
     const callBackURL = "https://mydomain.com/pat";
 
-    const timestamp =
+    const timestamp = (
         date.getFullYear() +
         ("0" + (date.getMonth() + 1)).slice(-2) +
         ("0" + date.getDate()).slice(-2) +
         ("0" + date.getHours()).slice(-2) +
-        ("0" + getMinutes()).slice(-2) +
-        ("0" + date.getSeconds()).slice(-2);
+        ("0" + date.getMinutes()).slice(-2) +
+        ("0" + date.getSeconds()).slice(-2)
+    );
 
-    const password = new Buffer.from(businessNumber + mpesaPassword + timestamp).toString("base64");
+    const passwordSaf = new Buffer.from(businessNumber + mpesaPassword + timestamp).toString("base64");
 
     const darajaRequestBody = {
-        "BusinessShortCode": tillNumber,
-        "Password": password,
+        "BusinessShortCode": businessNumber,
+        "Password": passwordSaf,
         "Timestamp": timestamp,
-        "TransactionType": "CustomerBuyGoodsOnline", //CustomerPayBillOnline(for Paybill) - CustomerBuyGoodsOnline(for till number)
+        "TransactionType": "CustomerPayBillOnline", //CustomerPayBillOnline(for Paybill) - CustomerBuyGoodsOnline(for till number)
         "Amount": amount,
         "PartyA": `254${phoneNumber}`,
-        "PartyB": tillNumber,
+        "PartyB": businessNumber,
         "PhoneNumber": `254${phoneNumber}`,
         "CallBackURL": callBackURL,
-        "AccountReference": `254${phoneNumber}`,
+        "AccountReference": "Test", //`254${phoneNumber}`
         "TransactionDesc": "Test" //Enter your randomly generated ticket numbers here.
     };
 
@@ -100,6 +103,6 @@ app.post("/stk", generateToken, async (req, res) => {
         res.status(200).json(data)
     }).catch((e) => {
         console.log(e.message);
-        res.status(400).json(err.message);
+        res.status(400).json(e.message);
     });
 });
