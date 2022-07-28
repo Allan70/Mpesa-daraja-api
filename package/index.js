a78b81021b8424374ad949ecb15acc96449d6cc6
Abere Allan <aabere70@gmail.com>
2022-07-29 00:12:35 +0300

    removed guiding comments

--- package/index.js
+++ package/index.js
@@ -58,17 +58,6 @@ app.post("/default_callback", generateToken, async (req, res) => {
     console.log(requestBody);
     console.log(metaData);
 
-    
-        // {
-        //    Item : [
-        //   { Name: 'Amount', Value: 1 },
-        //   { Name: 'MpesaReceiptNumber', Value: 'QGS69QI18Y' },
-        //   { Name: 'TransactionDate', Value: 20220728233053 },
-        //   { Name: 'PhoneNumber', Value: 254712765337 }
-        // ]
-        // }
-      
-
     const receipt = {
         "Amount": metaData.Item[0].Value,
         "mpesaReceiptNumber": metaData.Item[1].Value,
