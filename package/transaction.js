253b3b7ad9938a17253873419af574a05c0d2d71
Abere Allan <aabere70@gmail.com>
2022-07-31 10:58:59 +0300

    code refactored
    - [ ] Add an exception when customer balance is low.

--- package/transaction.js
+++ package/transaction.js
@@ -2,26 +2,54 @@ const transaction = {
     getTransactionInfo:(request)=>{
         
     const requestBody = request.body;
-    const metaData = request.body.Body.stkCallback.CallbackMetadata;
+    let metaData = request.body.Body.stkCallback.CallbackMetadata;
+    const transactionStatus = requestBody.Body.stkCallback.ResultDesc;
+    
+    let receipt = null;
+    let transactionInfo = null;
 
-    const receipt = {
+    if(!metaData){
+        receipt = {
+            "message": "Transaction was unsuccessful"
+        }
+
+        transactionInfo = {
+            "transactionStatus": transactionStatus,
+            "receipt": receipt
+        }
+    
+        return transactionInfo;
+    }
+    
+    if(metaData){
+    receipt = {
         "Amount": metaData.Item[0].Value,
         "mpesaReceiptNumber": metaData.Item[1].Value,
         "transactionDate": metaData.Item[2].Value,
         "phoneNumber": metaData.Item[3].Value
     }
 
-    const transactionStatus = requestBody.Body.stkCallback.ResultDesc;
-
-     
-    console.log(receipt);
-    console.log(transactionStatus);
-
-    const transactionInfo = {
+    transactionInfo = {
         "transactionStatus": transactionStatus,
         "receipt": receipt
     }
 
     return transactionInfo;
+
+    }
+
+    // Handle the no account ballance exception
+    if(!metaData && transactionStatus){
+        transactionInfo = {
+            "transactionStatus": transactionStatus,
+            "message": "Customer has no funds"
+        }
+    
+        return transactionInfo;
+    
+    }
+   
     }
 }
+
+module.exports = transaction;
