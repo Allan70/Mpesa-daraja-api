8a700f7479a88b3eb438cced321439419d491b81
Abere Allan <aabere70@gmail.com>
2022-08-01 20:18:50 +0300

    removed index.js

--- test/generateToken.test.js
+++ test/generateToken.test.js
@@ -1,3 +1,22 @@
-test('Generate a token to be used by M-Pesa',()=>{
-    expect().toBe()
-})
+const generateToken = require('../package/generateToken')
+
+test('Generate a token to be used by M-Pesa', async () => {
+    const secret = process.env.MPESA_SECRET_KEY;
+    const consumerKey = process.env.MPESA_CONSUMER_KEY;
+
+    const auth = new Buffer.from (`${consumerKey}:${secret}`).toString("base64");
+
+    await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials", {
+        headers: {
+            "Authorization": `Basic ${auth}`
+        },
+    }).then((response) => {
+        // console.log(response.data.access_token);
+       return token = response.data.access_token;
+    }).catch((err) => {
+        console.log(err);
+        res.status(400).json(err.message);
+    })
+
+    expect(generateToken()).toBe(token)
+});
